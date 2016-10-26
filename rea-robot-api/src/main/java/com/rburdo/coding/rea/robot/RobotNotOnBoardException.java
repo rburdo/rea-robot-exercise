1674886256f8af40e625782bc26ade402a8052f0
Ron Burdo <rburdo69@yahoo.com>
2016-10-27 03:02:43 +1100

    Delete files from obsolete repository

--- rea-robot-api/src/main/java/com/rburdo/coding/rea/robot/RobotNotOnBoardException.java
+++
@@ -1,8 +0,0 @@
-package com.rburdo.coding.rea.robot;
-
-
-public class RobotNotOnBoardException extends RobotException {
-    public RobotNotOnBoardException() {
-        super("Robot has not been placed on board yet");
-    }
-}
--- rea-robot-api/src/main/java/com/rburdo/coding/rea/robot/RobotOperator.java
+++
@@ -1,8 +0,0 @@
-package com.rburdo.coding.rea.robot;
-
-
-public interface RobotOperator {
-
-    void batchExecute() throws RobotException;
-
-}
